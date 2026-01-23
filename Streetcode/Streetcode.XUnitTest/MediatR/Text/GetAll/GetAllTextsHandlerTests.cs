b0b1810c480809146915b05f06d4e8679f61d81c
Darsicl <scorpion20022007@gmail.com>
2026-01-23 14:15:50 +0100

    Created first test for textGetAll

--- Streetcode/Streetcode.XUnitTest/MediatR/Text/GetAll/GetAllTextsHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatR/Text/GetAll/GetAllTextsHandlerTests.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="GetAllTextsHandlerTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Text;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.Text.GetAll;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Xunit;
 
 namespace Streetcode.XUnitTest.MediatR.Text.GetAll
 {
-    internal class GetAllTextsHandlerTests
+}
+
+public class GetAllTextsHandlerTests
+{
+    private readonly Mock<IRepositoryWrapper> mockRepoWrapper;
+    private readonly Mock<IMapper> mockMapper;
+    private readonly Mock<ILoggerService> mockLogger;
+    private readonly GetAllTextsHandler handler;
+
+    public GetAllTextsHandlerTests()
     {
+        this.mockRepoWrapper = new Mock<IRepositoryWrapper>();
+        this.mockMapper = new Mock<IMapper>();
+        this.mockLogger = new Mock<ILoggerService>();
+
+        this.handler = new GetAllTextsHandler(
+            this.mockRepoWrapper.Object,
+            this.mockMapper.Object,
+            this.mockLogger.Object);
+    }
+
+    [Fact]
+    public async Task Hadler_ShouldReturnOk_WhenTextsExist()
+    {
+        // Arrange
+        var textsList = new List<Text> { new Text { Id = 1 }, new Text { Id = 2 } };
+        var textsListDTOs = new List<TextDTO> { new TextDTO { Id = 1 }, new TextDTO { Id = 2 } };
+
+        mockRepoWrapper
+            .Setup(r => r.TextRepository.GetAllAsync(null, null))
+            .ReturnsAsync(textsList);
+
+        mockMapper
+            .Setup(m => m.Map<IEnumerable<TextDTO>>(textsList))
+            .Returns(textsListDTOs);
+
+        // Act
+        var result = await handler.Handle(new GetAllTextsQuery(), CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().HaveCount(2);
     }
 }
--- Streetcode/Streetcode.XUnitTest/MediatR/Text/GetById/GetTextByIdHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatR/Text/GetById/GetTextByIdHandlerTests.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using FluentAssertions;
+using FluentResults;
+using Moq;
+using Streetcode.BLL.DTO.Streetcode.TextContent.Text;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.Text.GetById;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.DAL.Repositories.Interfaces.Streetcode.TextContent;
+using Xunit;
 
-namespace Streetcode.XUnitTest.MediatR.Text.GetById
+namespace Streetcode.BLL.Tests.MediatR.Streetcode.Text;
+
+public class GetTextByIdHandlerTests
 {
-    internal class GetTextByIdHandlerTests
-    {
-    }
+
 }
