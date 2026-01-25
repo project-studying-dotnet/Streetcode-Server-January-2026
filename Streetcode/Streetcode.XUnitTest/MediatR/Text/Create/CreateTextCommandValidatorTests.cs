cea44457af11feba5247b777332221739e9f1f73
Darsicl <scorpion20022007@gmail.com>
2026-01-25 13:49:20 +0100

    test: Add test for CreateTextCommandValidator

--- Streetcode/Streetcode.XUnitTest/MediatR/Text/Create/CreateTextCommandValidatorTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatR/Text/Create/CreateTextCommandValidatorTests.cs
@@ -18,7 +18,7 @@ namespace Streetcode.XUnitTest.MediatR.Text.Create
         }
 
         [Fact]
-        public void Should_Have_Error_When_Text_Is_Null()
+        public void ShouldHaveError_WhenTextIsNull()
         {
             // Arrange
             var command = new CreateTextCommand(null);
@@ -32,7 +32,7 @@ namespace Streetcode.XUnitTest.MediatR.Text.Create
         }
 
         [Fact]
-        public void Should_Pass_When_Text_Is_Valid_Without_Video()
+        public void ShouldPass_WhenTextIsValid()
         {
             // Arrange
             var dto = new TextCreateDTO
@@ -40,7 +40,7 @@ namespace Streetcode.XUnitTest.MediatR.Text.Create
                 Title = "Test title",
                 TextContent = "Some content",
                 AdditionalText = "Additional",
-                StreetcodeId = 1,
+                StreetcodeId = 123,
             };
 
             var command = new CreateTextCommand(dto);
@@ -50,26 +50,7 @@ namespace Streetcode.XUnitTest.MediatR.Text.Create
 
             // Assert
             result.ShouldNotHaveAnyValidationErrors();
-        }
 
-        [Fact]
-        public void Should_Pass_When_Text_Is_Valid_With_Youtube_Video()
-        {
-            // Arrange
-            var dto = new TextCreateDTO
-            {
-                Title = "Test title",
-                TextContent = "Some content",
-                StreetcodeId = 1,
-            };
-
-            var command = new CreateTextCommand(dto);
-
-            // Act
-            var result = _validator.TestValidate(command);
-
-            // Assert
-            result.ShouldNotHaveAnyValidationErrors();
         }
     }
 }
