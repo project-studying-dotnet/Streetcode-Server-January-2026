24572a1095600739078ecccf87d321f3a9597eb1
Max668103 <skydanchukm@gmail.com>
2026-01-30 02:52:22 +0200

    feat/SSAD-0039: added Order property and created migration

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20260130002946_AddOrderPropertyToFacts.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderPropertyToFacts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StreetcodeType",
+                schema: "streetcode",
+                table: "streetcodes",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                schema: "streetcode",
+                table: "facts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoordinateType",
+                schema: "add_content",
+                table: "coordinates",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                schema: "streetcode",
+                table: "facts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StreetcodeType",
+                schema: "streetcode",
+                table: "streetcodes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoordinateType",
+                schema: "add_content",
+                table: "coordinates",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
