fd8ba10d45d86860cbe5cc5517d7d0ac3bfef7ba
Darsicl <scorpion20022007@gmail.com>
2026-02-05 14:11:15 +0100

    Creat validation behavior in MediatR

---
+++ Streetcode/Streetcode.BLL/MediatR/ValidatorBehavior.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Streetcode.BLL.MediatR
+{
+    public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidatorBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/TextController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/TextController.cs
@@ -39,19 +39,19 @@ public class TextController : BaseApiController
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateText([FromBody] TextCreateDTO textBaseDTO)
+    public async Task<IActionResult> CreateText([FromBody] TextCreateDTO textCreateDTO)
     {
-        return HandleResult(await Mediator.Send(new CreateTextCommand(textBaseDTO)));
+        return HandleResult(await Mediator.Send(new CreateTextCommand(textCreateDTO)));
     }
 
     [HttpPut]
-    public async Task<IActionResult> UpdateText([FromBody] TextUpdateDTO textBaseDTO)
+    public async Task<IActionResult> UpdateText([FromBody] TextUpdateDTO textUpdateDTO)
     {
-        return HandleResult(await Mediator.Send(new UpdateTextCommand(textBaseDTO)));
+        return HandleResult(await Mediator.Send(new UpdateTextCommand(textUpdateDTO)));
     }
 
-    [HttpDelete]
-    public async Task<IActionResult> DeleteText([FromQuery] int id)
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteText([FromRoute] int id)
     {
         return HandleResult(await Mediator.Send(new DeleteTextCommand(id)));
     }
