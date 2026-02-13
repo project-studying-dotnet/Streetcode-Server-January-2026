82914359b4c7e9c36a633dbd222a9c20113a9747
Darsicl <scorpion20022007@gmail.com>
2026-02-13 14:55:36 +0100

    refactor: preparing for merge

--- Streetcode/Streetcode.WebApi/Middleware/GlobalExceptionHandlingMiddleware.cs
+++ Streetcode/Streetcode.WebApi/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -9,9 +9,7 @@ namespace Streetcode.WebApi.Middleware
         private readonly RequestDelegate _next;
         private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
 
-        public GlobalExceptionHandlingMiddleware(
-            RequestDelegate next,
-            ILogger<GlobalExceptionHandlingMiddleware> logger)
+        public GlobalExceptionHandlingMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlingMiddleware> logger)
         {
             _next = next;
             _logger = logger;
