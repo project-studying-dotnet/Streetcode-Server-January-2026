846b21c945f502dd0b6ee28817fc1ebbee705b05
Darsicl <scorpion20022007@gmail.com>
2026-02-18 13:04:17 +0100

    feat: finish HistoricalContext delete

---
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/HistoricalContext/Update/HistoricalContextDTOUpdateValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.MediatR.Timeline.HistoricalContext.Update
+{
+    public class HistoricalContextDTOUpdateValidator : AbstractValidator<HistoricalContextDTOUpdate>
+    {
+    }
+}
---
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/HistoricalContext/Update/UpdateHistoricalContextHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.Update
+{
+    internal class UpdateTimelineItemHandler
+    {
+    }
+}
