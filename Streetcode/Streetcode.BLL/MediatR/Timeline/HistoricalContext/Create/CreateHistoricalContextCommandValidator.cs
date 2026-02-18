9407c488bedf9bcd834180d5e2b751d6d43eca45
Darsicl <scorpion20022007@gmail.com>
2026-02-18 12:46:28 +0100

    feat: finish HistoricalContext create

---
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/HistoricalContext/Create/CreateHistoricalContextCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Streetcode.Resources;
+
+namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.Create
+{
+    public class CreateTimelineItemCommandValidator : AbstractValidator<CreateHistoricalContextCommand>
+    {
+        public CreateTimelineItemCommandValidator()
+        {
+            RuleFor(x => x.TimelineItem)
+                .NotNull()
+                .WithMessage(Messages.Error_CommandDataRequired)
+                .SetValidator(new TimelineItemCreateDTOValidator());
+        }
+    }
+}
