857b270eb7a0e0c8fcaa11aa143e11e52e3565ae
Max668103 <skydanchukm@gmail.com>
2026-02-20 22:17:41 +0200

    feat/SSAD-0104: added comment entity

--- Streetcode/Streetcode.DAL/Entities/Users/User.cs
+++ Streetcode/Streetcode.DAL/Entities/Users/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Streetcode.DAL.Entities.Streetcode.Comments;
 using Streetcode.Shared.Enums;
 
 namespace Streetcode.DAL.Entities.Users
@@ -10,18 +11,25 @@ namespace Streetcode.DAL.Entities.Users
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Id { get; set; }
+
         [Required]
         [MaxLength(100)]
         public string Name { get; set; }
+
         [Required]
         [MaxLength(150)]
         public string Surname { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         public string PhoneNumber { get; set; }
+
         [Required]
         public UserRole Role { get; set; }
+
+        public List<Comment>? Comments { get; set; }
     }
 }
