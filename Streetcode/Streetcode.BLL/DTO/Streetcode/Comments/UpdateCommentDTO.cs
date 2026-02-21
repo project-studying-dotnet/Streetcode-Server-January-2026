ff82862fd4a4624e9efbfded4eafb476671d64e8
Max668103 <skydanchukm@gmail.com>
2026-02-21 21:23:53 +0200

    feat/SSAD-0104: added dto and mapper profile

---
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/Comments/UpdateCommentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Streetcode.BLL.DTO.Streetcode.Comments
+{
+    public class UpdateCommentDTO
+    {
+        public int Id { get; set; }
+
+        public string TextContent { get; set; }
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Mapping/Streetcode/Comments/CommentProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Streetcode.BLL.DTO.Streetcode.Comments;
+using Streetcode.DAL.Entities.Streetcode.Comments;
+
+namespace Streetcode.BLL.Mapping.Streetcode.Comments
+{
+    public class CommentProfile : Profile
+    {
+        public CommentProfile()
+        {
+            CreateMap<Comment, CommentDTO>()
+                .ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => 
+                    src.User != null ? $"{src.User.Name} {src.User.Surname}" : "Unknown User"))
+                .ReverseMap();
+
+            CreateMap<CreateCommentDTO, Comment>();
+            CreateMap<UpdateCommentDTO, Comment>();
+        }
+    }
+}
