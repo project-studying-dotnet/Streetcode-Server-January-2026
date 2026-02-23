505c697325875eee3db6f2368c89c7b70e2de12c
Darsicl <scorpion20022007@gmail.com>
2026-02-23 13:56:08 +0100

    fix:for TimelineItem change createDTO on UpdateDTO in updateValidator

--- Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/Update/TimelineItemUpdateDTOValidator.cs
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/Update/TimelineItemUpdateDTOValidator.cs
@@ -15,9 +15,9 @@ namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.Update
 
             RuleFor(x => x.Title)
                     .NotEmpty()
-                    .WithMessage(Messages.Error_PropertyIsRequired.Format(nameof(CreateTimelineItemDTO.Title)))
+                    .WithMessage(Messages.Error_PropertyIsRequired.Format(nameof(UpdateTimelineItemDTO.Title)))
                     .MaximumLength(100)
-                    .WithMessage(Messages.Error_PropertyMustNotExceedCharacters.Format(nameof(CreateTimelineItemDTO.Title), 100));
+                    .WithMessage(Messages.Error_PropertyMustNotExceedCharacters.Format(nameof(UpdateTimelineItemDTO.Title), 100));
 
             RuleFor(x => x.Description)
                 .MaximumLength(600)
@@ -26,19 +26,19 @@ namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.Update
 
             RuleFor(x => x.Date)
                 .NotEmpty()
-                .WithMessage(Messages.Error_PropertyIsRequired.Format(nameof(CreateTimelineItemDTO.Date)));
+                .WithMessage(Messages.Error_PropertyIsRequired.Format(nameof(UpdateTimelineItemDTO.Date)));
 
             RuleFor(x => x.DateViewPattern)
                 .IsInEnum()
-                .WithMessage(string.Format(Messages.Error_EntitiesNotFound, nameof(CreateTimelineItemDTO.DateViewPattern)));
+                .WithMessage(string.Format(Messages.Error_EntitiesNotFound, nameof(UpdateTimelineItemDTO.DateViewPattern)));
 
             RuleFor(x => x.StreetcodeId)
                 .GreaterThan(0)
-                .WithMessage(Messages.Error_PropertyMustBeGreaterThanZero.Format(nameof(CreateTimelineItemDTO.StreetcodeId)));
+                .WithMessage(Messages.Error_PropertyMustBeGreaterThanZero.Format(nameof(UpdateTimelineItemDTO.StreetcodeId)));
 
             RuleForEach(x => x.HistoricalContextIds)
                 .GreaterThan(0)
-                .WithMessage(Messages.Error_PropertyMustBeGreaterThanZero.Format(nameof(CreateTimelineItemDTO.HistoricalContextIds)));
+                .WithMessage(Messages.Error_PropertyMustBeGreaterThanZero.Format(nameof(UpdateTimelineItemDTO.HistoricalContextIds)));
         }
     }
 }
