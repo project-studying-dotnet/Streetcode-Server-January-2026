2f46d9c3d5695f1ff798a3c7d4cb13837d9b2edc
Darsicl <scorpion20022007@gmail.com>
2026-02-25 17:55:57 +0100

    feat: create appsetings

--- Streetcode/Streetcode.Email.WebAPI/Extensions/ServiceCollectionExtensions.cs
+++ Streetcode/Streetcode.Email.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,65 @@
-﻿using Streetcode.Email.BLL.Configs;
+﻿using Hangfire;
+using Microsoft.EntityFrameworkCore;
+using Streetcode.Email.BLL.Configs;
 using Streetcode.Email.BLL.Interfaces;
 using Streetcode.Email.BLL.Services;
+using Streetcode.Email.DAL.Persistence;
 
 namespace Streetcode.Email.WebAPI.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        public static void AddApplicationServices(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var emailConfig = configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+            services.AddSingleton(emailConfig);
+
+            services.AddDbContext<EmailDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, opt =>
+                {
+                    opt.MigrationsAssembly(typeof(EmailDbContext).Assembly.GetName().Name);
+                    opt.MigrationsHistoryTable("__EFMigrationsHistory", schema: "entity_framework");
+                });
+            });
+
+            services.AddHangfire(config =>
+            {
+                config.UseSqlServerStorage(connectionString);
+            });
+
+            services.AddHangfireServer();
+
+            var corsConfig = configuration.GetSection("CORS").Get<CorsConfiguration>();
+            services.AddCors(opt =>
+            {
+                opt.AddDefaultPolicy(policy =>
+                {
+                    if (corsConfig?.AllowedOrigins?.Any() == true && !corsConfig.AllowedOrigins.Contains("*"))
+                    {
+                        policy.WithOrigins(corsConfig.AllowedOrigins.ToArray());
+                    }
+                    else
+                    {
+                        policy.SetIsOriginAllowed(origin => true);
+                    }
+
+                    policy.AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            services.AddHsts(opt =>
+            {
+                opt.Preload = true;
+                opt.IncludeSubDomains = true;
+                opt.MaxAge = TimeSpan.FromDays(30);
+            });
+
+            services.AddLogging();
+            services.AddControllers();
+        }
         public static void AddCustomServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<EmailConfiguration>(
