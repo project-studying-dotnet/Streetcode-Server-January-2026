10a4efe77a3e6885a6a7c4fa76fc02962ff66404
Darsicl <scorpion20022007@gmail.com>
2026-03-03 16:12:29 +0100

    feat: create google login

---
+++ Streetcode/Streetcode.Auth.BLL/MediatR/LoginWithGoogle/LoginWithGoogleCommandValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Streetcode.Auth.BLL.MediatR.Login;
+
+namespace Streetcode.Auth.BLL.MediatR.LoginWithGoogle
+{
+    public class LoginWithGoogleCommandValidation : AbstractValidator<LoginWithGoogleCommand>
+    {
+        public LoginWithGoogleCommandValidation()
+        {
+            RuleFor(x => x.LoginGoogle)
+                .SetValidator(new LoginWithGoogleDTOValidator());
+        }
+    }
+}
