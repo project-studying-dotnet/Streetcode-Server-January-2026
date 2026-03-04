b9766e0a15df229041bacaee32f88fccccfe8241
Darsicl <scorpion20022007@gmail.com>
2026-03-04 12:21:45 +0100

    feat: add mappin for LoginWith Google

--- Streetcode/Streetcode.Auth.BLL/Mapping/AuthProfile.cs
+++ Streetcode/Streetcode.Auth.BLL/Mapping/AuthProfile.cs
@@ -13,6 +13,12 @@ namespace Streetcode.Auth.BLL.Mapping
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email));
 
             CreateMap<ApplicationUser, UserDTO>();
+
+            CreateMap<LoginWithGoogleDTO, ApplicationUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.EmailConfirmed, opt => opt.MapFrom(_ => true));
         }
     }
 }
